0d9d122262b705bbde1d65a88839717f1e8fe1d2
Tom Porvaznik <tomporvaz@gmail.com>
2019-03-10 21:20:58 -0400

    Added functions to handle number input, inserting operators, and calculating results, and clearing display

--- src/App.js
+++ src/App.js
@@ -3,46 +3,107 @@ import logo from './logo.svg';
 import './App.css';
 import math from "mathjs";
 
+let numbers = [
+  "one",
+  "two",
+  "three", 
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine"
+]
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      inputArr: [],
+      inputNumArr: [],
       inputNum: 0,
       calcArr: [],
-      calcString:"Let's calculate!"
+      calcString:"Let's calculate!",
+      total: 0
 
     }
-    
+    this.handleInput = this.handleNumInput.bind(this);
+    this.handleOperator = this.handleOperator.bind(this);
+    this.handleClear = this.handleClear.bind(this);
+    this.handleCalculation = this.handleCalculation.bind(this);
   }
-  /*event handler for number input
-  handleNumInput(){
-    this.setState(
-      inputArr
-    )
-    
+  /*event handler for number inputs*/
+  handleNumInput(input){
+    let arr =[...this.state.inputNumArr, input];
+    this.setState({
+      inputNumArr: arr,
+      total: ""
+    }) 
   }
-  */
-  
-  
-  
+
+  /*event handler for operations*/
+  handleOperator(operation){
+    let arr = [...this.state.calcArr, this.state.inputNumArr.join(""), this.state.total, operation];
+    this.setState({
+      inputNumArr: [],
+      calcArr: arr,
+      total: ""
+    })
+  }
+
+  /*event handler for clear all*/
+  handleClear(){
+    this.setState({
+      inputNumArr: [],
+      calcArr: [],
+      total: 0
+    })
+  }
+
+  /*event handler to calculate the total*/
+  handleCalculation(){
+    
+    let newArr = [...this.state.calcArr, this.state.inputNumArr.join("")];
+    let total = math.eval(newArr.join(""));
+    let displayArr = [...this.state.calcArr, "=", total];
+
+    this.setState({
+      inputNumArr: [],
+      calcArr: [],
+      total: total
+    })
+  }
+
+  /*didUpdate for console log debugging*/
+  componentDidUpdate(){
+    console.log("inputNumArr: " + this.state.inputNumArr);
+    console.log("calcArr: " + this.state.calcArr);
+    console.log("total: " + this.state.total);
+  }
+
+
+
+
+
+
+  renderButtons = numbers.map((number, index) => <button id={number} onClick={() => this.handleNumInput((index + 1))}>{index + 1}</button>);
+    
   render() {
     return (
       <div className="App">
         <div id="calculator">
-          <p id="display">{this.state.calcString}</p>
+          <p id="display">{this.state.calcArr}{this.state.inputNumArr}{this.state.total}</p>
           <div id="btnContainer">
-            <button id='clear'>CA</button>
-                        <button id='divide'>/</button>
-            <button id='multiply'>*</button>
-            <button id='subtract'>-</button>
+            <button id='clear' onClick={this.handleClear}>AC</button>
+            <button id='divide' onClick={() => this.handleOperator("/")}>/</button>
+            <button id='multiply'onClick={() => this.handleOperator("*")}>*</button>
+            <button id='subtract'onClick={() => this.handleOperator("-")}>-</button>
             <div id="numBtnsContainer">
-              <NumBtns/>
+              {this.renderButtons}
             </div>              
-            <button id='add'>+</button>
-            <button id='zero'>0</button>
-            <button id='decimal'>.</button>
-            <button id='equals'>=</button>
+            <button id='add'onClick={() => this.handleOperator("+")}>+</button>
+            <button id='zero' onClick={() => this.handleNumInput(0)}>0</button>
+            <button id='decimal'onClick={() => this.handleNumInput(".")}>.</button>
+            <button id='equals' onClick={this.handleCalculation}>=</button>
           </div>
 
         </div>
@@ -68,32 +129,11 @@ class App extends Component {
   
   let arr = [1, 2, 3, 4];
   let newArr = arr.join("");
-  let calcArr = [newArr, "-", 3];
+  let calcArr = [2, "/", 7];
   let calcString = calcArr.join("");
   
-  console.log(math.eval(calcString));
+  console.log(math.eval(calcArr.join("")));
   
-  //number button component
-  
-  function NumBtns() {
-    let numbers = [
-      "one",
-      "two",
-      "three",
-      "four",
-      "five",
-      "six",
-      "seven",
-      "eight",
-      "nine"
-    ]
-    const renderButtons = numbers.map((number, index) => <button id={number}>{index + 1}</button>);
-    return renderButtons;
-  }
-  
-  
-  
-  
-  
+   
   export default App;
   

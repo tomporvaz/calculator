ce08b2b0bb2bf1fb4d06e1dc56e583774a8cfb08
Tom Porvaznik <tomporvaz@gmail.com>
2019-03-14 20:42:56 -0400

    Updates

---
+++ precache-manifest.9c073d9b524714f6436dbf61903cce23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d721d06f78c96d55f57",
+    "url": "/calculator/static/js/runtime~main.19f1d22d.js"
+  },
+  {
+    "revision": "3185e0f973db3d066dcf",
+    "url": "/calculator/static/js/main.42c9a311.chunk.js"
+  },
+  {
+    "revision": "0eeda583971f8acfc9fa",
+    "url": "/calculator/static/js/2.e4014699.chunk.js"
+  },
+  {
+    "revision": "3185e0f973db3d066dcf",
+    "url": "/calculator/static/css/main.d8b9c6fe.chunk.css"
+  },
+  {
+    "revision": "6a14523a392ae3105465689f4263bdb2",
+    "url": "/calculator/index.html"
+  }
+];
